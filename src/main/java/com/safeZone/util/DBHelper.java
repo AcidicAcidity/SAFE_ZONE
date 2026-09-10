a2992ece32cb291adc97267cec8fa37256d70ce5
Gialde <denis.kondratenko307@gmail.com>
2026-09-10 17:43:29 +0300

    исправил ошибки в методе для проверки авторизации + написал медот для регестрации

--- src/main/java/com/safeZone/util/DBHelper.java
+++ src/main/java/com/safeZone/util/DBHelper.java
@@ -73,16 +73,17 @@ public class DBHelper {
     }
 
     // ПРОВЕРКА НА АВТОРИЗАЦИЮ
-    public boolean userAuth(String username, String password) throws SQLException {
-        if (username == null || username.isBlank() ||
+    public boolean userAuth(String login, String password) throws SQLException {
+        if (login == null || login.isBlank() ||
             password == null || password.isBlank()) {
+            log.error("Неверный логин или пароль");
             return false;
         }
         String sql = "SELECT 1 FROM users WHERE login = ? AND password =? LIMIT 1";
         try (Connection conn = dbUtils.getConnection();
              PreparedStatement ps = conn.prepareStatement(sql)) {
             
-            ps.setString(1,username);
+            ps.setString(1,login);
             ps.setString(2,password);
             try (ResultSet rs = ps.executeQuery()){
                 return rs.next();
@@ -93,9 +94,30 @@ public class DBHelper {
             return false;
         }
     }
+// Добавление пользователя в базу данных
+    public void addUser(String login, String password) throws SQLException  {
+        if (login == null || login.isBlank()||
+            password == null || password.isBlank()){
+            log.error("Пустой логин или пароль — пользователь не добавлен");
+            return;
+            }
+        String sql = "INSERT INTO users (login, password, status, role) VALUES (?,?,active,client)";
+        try (Connection conn = dbUtils.getConnection();
+             PreparedStatement ps = conn.prepareStatement(sql)) {
+            
+            ps.setString(1,login);
+            ps.setString(2,password);
 
-    public void addUser(String username, String password)  {
-        // Добавление пользователя в базу данных
+            ps.executeUpdate();
+            log.info("Вы успешно зарегестрированны!");
+
+        } catch (SQLException e) {
+            if ("23505".equals(e.getSQLState())){
+                log.error("Такой логин '{}' уже занят");
+            }else{
+                log.error("Ошибка добавления пользователя: {}", e.getMessage(), e);
+            }
+        }
     }
 
     public int createPayment(LocalDateTime rentTime, String size, int binId, String token) {
