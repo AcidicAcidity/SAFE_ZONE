5c372d93a3c008f8d6cc28ee6e24761f4f97d7fc
AcidicAcidity <egoralexandrov131044@gmail.com>
2026-09-10 12:57:26 +0300

    Fill empty methods in views

--- src/main/java/com/safeZone/views/PaymentMenu.java
+++ src/main/java/com/safeZone/views/PaymentMenu.java
@@ -13,5 +13,50 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class PaymentMenu {
+    private static WindowBasedTextGUI gui;
+    private DBHelper dbHelper;
+    private AppMenus appMenus;
+    private Logger log = LoggerFactory.getLogger(PaymentMenu.class);
 
+    public PaymentMenu(DBHelper dbHelper, AppMenus appMenus) {
+        this.dbHelper = dbHelper;
+        this.appMenus = appMenus;
+    }
+
+    public void paymentWindow() {
+        BasicWindow paymentWindow = new BasicWindow("Платежи");
+        Panel panel = new Panel(new LinearLayout(Direction.VERTICAL));
+        Panel filterPanel = new Panel(new LinearLayout(Direction.HORIZONTAL));
+
+        TextBox binId = new TextBox(new TerminalSize(30, 2));
+        binId.setText("Bin ID");
+        TextBox ownerId = new TextBox(new TerminalSize(30, 2));
+        ownerId.setText("Owner ID");
+        TextBox amount = new TextBox(new TerminalSize(30, 2));
+        amount.setText("Amount");
+        TextBox rentTime = new TextBox(new TerminalSize(30, 2));
+        rentTime.setText("Rent Time");
+        TextBox status = new TextBox(new TerminalSize(30, 2));
+        status.setText("Status");
+
+        Button search = new Button("Поиск", () -> {
+            int binIdValue = Integer.parseInt(binId.getText());
+            int ownerIdValue = Integer.parseInt(ownerId.getText());
+            int amountValue = Integer.parseInt(amount.getText());
+            int rentTimeValue = Integer.parseInt(rentTime.getText());
+            String statusValue = status.getText();
+            // TODO: БД поиск платежей
+        });
+
+        filterPanel.addComponent(binId);
+        filterPanel.addComponent(ownerId);
+        filterPanel.addComponent(amount);
+        filterPanel.addComponent(rentTime);
+        filterPanel.addComponent(status);
+        filterPanel.addComponent(search);
+        panel.addComponent(filterPanel);
+
+        paymentWindow.setComponent(panel);
+        gui.addWindowAndWait(paymentWindow);
+    }
 }
--- src/main/java/com/safeZone/views/StatsMenu.java
+++ src/main/java/com/safeZone/views/StatsMenu.java
@@ -13,5 +13,23 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class StatsMenu {
+    private Logger log = LoggerFactory.getLogger(StatsMenu.class);
+    private static WindowBasedTextGUI gui;
+    private DBHelper dbHelper;
+    private AppMenus appMenus;
 
+    public StatsMenu(DBHelper dbHelper, AppMenus appMenus) {
+        this.dbHelper = dbHelper;
+        this.appMenus = appMenus;
+    }
+
+    public void showStats() {
+        BasicWindow statsWindow = new BasicWindow("Статистика ячеек");
+        Panel panel = new Panel(new LinearLayout(Direction.VERTICAL));
+
+        // тут запрос в БД где мы берем ячейку и пробиваем все платежи по этой ячейке.
+        // Отдельно ищем самую длинную аренду и текущий статус ячейки
+        //
+        gui.addWindowAndWait(statsWindow);
+    }
 }
