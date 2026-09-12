776dd572802c4c853ff490b4dcf3c549549cabe9
Gialde <denis.kondratenko307@gmail.com>
2026-09-12 17:49:45 +0300

    Хз чо за комин, гит мне его сделал

--- src/main/java/com/safeZone/util/DBHelper.java
+++ src/main/java/com/safeZone/util/DBHelper.java
@@ -9,6 +9,8 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.time.LocalDateTime;
+
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -69,20 +71,73 @@ public class DBHelper {
             return ps.executeUpdate();
         }
     }
-    /**
-     * INSERT с RETURNING — возвращает сгенерированный id.
-     * Для PostgreSQL: "INSERT INTO ... RETURNING id".
-     */
-    public int executeInsertReturning(String sql, Object... params) throws SQLException{
+
+    // ПРОВЕРКА НА АВТОРИЗАЦИЮ
+    public boolean userAuth(String login, String password) throws SQLException {
+        if (login == null || login.isBlank() ||
+            password == null || password.isBlank()) {
+            log.error("Неверный логин или пароль");
+            return false;
+        }
+        String sql = "SELECT 1 FROM users WHERE login = ? AND password =? LIMIT 1";
         try (Connection conn = dbUtils.getConnection();
-        PreparedStatement ps = conn.prepareStatement(sql)){
-            for (int i = 0; i < params.length; i++){
-                ps.setObject(i + 1, params[i]);
-            }
+             PreparedStatement ps = conn.prepareStatement(sql)) {
+            
+            ps.setString(1,login);
+            ps.setString(2,password);
             try (ResultSet rs = ps.executeQuery()){
-            if (rs.next()) return rs.getInt(1);
+                return rs.next();
+            }
+
+        } catch (SQLException e) {
+            log.error("Ошибка подключения к БД: {}", e.getMessage(), e);
+            return false;
+        }
+    }
+// Добавление пользователя в базу данных
+    public void addUser(String login, String password) throws SQLException  {
+        if (login == null || login.isBlank()||
+            password == null || password.isBlank()){
+            log.error("Пустой логин или пароль — пользователь не добавлен");
+            return;
+            }
+        String sql = "INSERT INTO users (login, password, status, role) VALUES (?,?,active,client)";
+        try (Connection conn = dbUtils.getConnection();
+             PreparedStatement ps = conn.prepareStatement(sql)) {
+            
+            ps.setString(1,login);
+            ps.setString(2,password);
+
+            ps.executeUpdate();
+            log.info("Вы успешно зарегестрированны!");
+
+        } catch (SQLException e) {
+            if ("23505".equals(e.getSQLState())){
+                log.error("Такой логин '{}' уже занят");
+            }else{
+                log.error("Ошибка добавления пользователя: {}", e.getMessage(), e);
             }
         }
-        return -1;
+    }
+
+    public int createPayment(LocalDateTime rentTime, String size, int binId, String token) {
+        // Создание платежа в базе данных
+        // Возвращает ID созданного платежа
+        return 0;
+    }
+
+    public String getPaymentStatus(int paymentId) {
+        // Получение статуса платежа
+        return null;
+    }
+
+    public List<Map<String, Object>> getPayments(LocalDateTime rentTime, LocalDateTime createdAt, LocalDateTime endRentDate, int binId, int userId) {
+        return null; // Поиск платежей с фильтрацией или все что есть
+    }
+
+    public int findFreeBin(String size, LocalDateTime rentTime) {
+        // Поиск свободного бинна для размера и времени аренды
+        // Возвращает ID найденной ячейки = Int(pos_x + pos_y)
+        return 0;
     }
 }
