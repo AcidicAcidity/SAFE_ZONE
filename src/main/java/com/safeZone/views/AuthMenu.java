1489b1ba92f7a2e035106db2949726bfb582b111
jxkk <leanzetik@gmail.com>
2026-09-21 05:49:28 +0300

    Fix build errors PaymentProvider - Fix error win int bindId by adding getBin_ID() method SafeZoneService - Fix authUser to return User object AuthMenu - Commented unused fields in AuthMenu ThemeTUI.properties - Removed "srg=", which caused a runtime error.

--- src/main/java/com/safeZone/views/AuthMenu.java
+++ src/main/java/com/safeZone/views/AuthMenu.java
@@ -1,13 +1,25 @@
 package com.safeZone.views;
 
-import com.googlecode.lanterna.gui2.*;
+import java.sql.SQLException;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.googlecode.lanterna.TextColor;
+import com.googlecode.lanterna.gui2.BasicWindow;
+import com.googlecode.lanterna.gui2.Button;
+import com.googlecode.lanterna.gui2.DefaultWindowManager;
+import com.googlecode.lanterna.gui2.Direction;
+import com.googlecode.lanterna.gui2.EmptySpace;
+import com.googlecode.lanterna.gui2.Label;
+import com.googlecode.lanterna.gui2.LinearLayout;
+import com.googlecode.lanterna.gui2.MultiWindowTextGUI;
+import com.googlecode.lanterna.gui2.Panel;
+import com.googlecode.lanterna.gui2.TextBox;
+import com.googlecode.lanterna.gui2.WindowBasedTextGUI;
 import com.googlecode.lanterna.gui2.dialogs.MessageDialog;
 import com.googlecode.lanterna.screen.Screen;
-import com.googlecode.lanterna.TextColor;
-import com.googlecode.lanterna.graphics.ThemeDefinition;
 import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
-import org.slf4j.*;
-import java.sql.SQLException;
 import com.safeZone.util.DBHelper;
 import com.safeZone.util.SafeZoneService;
 
@@ -18,10 +30,13 @@ public class AuthMenu {
     private SafeZoneService service;
     private static WindowBasedTextGUI gui;
 
-    public AuthMenu(DBHelper dbHelper, AppMenus appMenus, SafeZoneService service ){
+    
+    public AuthMenu(DBHelper dbHelper){
         this.dbHelper = dbHelper;
-        this.appMenus = appMenus;
-        this.service = service;
+        // TODO Нигде не используется, но мешало сборке
+        // Потом раскомментим если понадобится
+        // this.appMenus = appMenus;
+        // this.service = service;
     }
 
     public void start() throws Exception {
@@ -30,7 +45,7 @@ public class AuthMenu {
                 .createScreen();
         screen.startScreen();
 
-        MultiWindowTextGUI gui = new MultiWindowTextGUI(
+        gui = new MultiWindowTextGUI(
             screen,
             new DefaultWindowManager(),
             new EmptySpace(TextColor.ANSI.BLACK)
@@ -82,7 +97,7 @@ public class AuthMenu {
 
             if ((username.isEmpty() == false) && (password.isEmpty() == false)) {
                 try {
-                    boolean isAuth = service.authUser(username, password);
+                    boolean isAuth = service.authUser(username, password) != null;
                     if ( isAuth == true) {
                         log.info("Authentication successful");
                         signInWindow.close();
